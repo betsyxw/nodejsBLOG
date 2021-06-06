ee58c4d0bdbd8762048bdfe297d50957294f731f
betsywenwen <betsywenwen@yahoo.co.jp>
2021-06-06 21:47:24 +0800

    main

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 const querystring = require('querystring')
 
+//获取cookie的过期时间
+const getCookieExpires = () =>{
+  const d = new Date()
+  d.setTime(d.getTime()+(24*60*60*1000))
+  console.log('d.toGMTString() is:', d.toGMTString())
+  return d.toGMTString()
+}
+
+//session数据
+const SESSION_DATA = {}
+
+
 //用于处理postdata,这里=js的callback，用原生自带promise函数.then
 const getPostData = (req) => {
   const promise = new Promise((resolve, reject)=>{
@@ -41,6 +53,38 @@ const serverHandle = (req,res)=>{
   //解析query,url后半部分
   req.query = querystring.parse(url.split('?')[1])
 
+  //解析cookie
+  req.cookie = {}
+  const cookieStr = req.headers.cookie || '' //k1=v1;k2=v2
+  cookieStr.split(';').forEach(item =>{
+    if(!item){
+      return
+    }
+    const arr = item.split('=')
+    const key = arr[0].trim()
+    const val = arr[1].trim()
+    req.cookie[key] = val
+  })
+  console.log('req.cookie is:',req.cookie)
+
+
+  //解析session
+  let needSetCookie = false
+  let userId = req.cookie.userid
+  if(userId){
+    if(!SESSION_DATA[userId]){
+      SESSION_DATA[userId] ={}
+    }
+  }else{
+    needSetCookie = true
+    userId = `${Date.now()}_${Math.random()}`
+    SESSION_DATA[userId] = {}
+  }
+  req.session = SESSION_DATA[userId]
+
+
+
+
   //处理路由之前，先解析post data
   getPostData(req).then(postData =>{
     req.body = postData
@@ -58,6 +102,11 @@ const serverHandle = (req,res)=>{
     const blogResult = handleBlogRouter(req,res)
     if(blogResult){
       blogResult.then(blogData =>{
+        //session--不行就删除--bug
+        // if(needSetCookie){
+        //   res.setHeader('Set-Cookie',`userid=${useId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        // }
+
         res.end(
           JSON.stringify(blogData)
         )
@@ -77,6 +126,11 @@ const serverHandle = (req,res)=>{
     const userResult = handleUserRouter(req,res)
     if(userResult){
       userResult.then(userData =>{
+        //session--不行就删除--bug
+        // if(needSetCookie){
+        //   res.setHeader('Set-Cookie',`userid=${useId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        // }
+
         res.end(JSON.stringify(userData))
       })
       return
